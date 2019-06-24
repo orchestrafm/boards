a5c29b20bde34d2a4890234dd112b4e7eee600f9
Starz0r <starz0r@starz0r.com>
2019-06-24 00:49:12 -0500

    database: find boards from primary key

---
+++ src/database/find.go
@@ -0,0 +1,26 @@
+package database
+
+import (
+	"database/sql"
+	"strconv"
+
+	"github.com/spidernest-go/logger"
+)
+
+func SelectID(id uint64) (*Board, error) {
+	tracks := db.Collection("boards")
+	rs := tracks.Find(id)
+	t := *new(Board)
+	err := rs.One(&t)
+	if err != nil && err != sql.ErrNoRows {
+		logger.Error().
+			Err(err).
+			Msg("Bad parameters or database error.")
+	}
+
+	if err == sql.ErrNoRows {
+		return nil, err
+	} else {
+		return &t, nil
+	}
+}
