a56fa17e8f9b84d09a39f0e96433d5d5b43339d5
Starz0r <starz0r@starz0r.com>
2019-06-24 01:56:47 -0500

    main: start the service from top to bottom

---
+++ src/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/orchestrafm/boards/src/database"
+	"github.com/orchestrafm/boards/src/routing"
+	"github.com/spidernest-go/logger"
+)
+
+func main() {
+	err := database.Connect()
+	logger.Error().
+		Err(err).
+		Msg("MySQL Database could not be attached to.")
+	database.Synchronize()
+
+	routers.ListenAndServe()
+}
