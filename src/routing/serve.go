f9d4e99705455391eaf49906d3d1dba5d6631cef
Starz0r <starz0r@starz0r.com>
2019-06-24 01:55:38 -0500

    routing: serve all api routes

---
+++ src/routing/serve.go
@@ -0,0 +1,23 @@
+package routers
+
+import (
+	"github.com/spidernest-go/mux"
+)
+
+var r *echo.Echo
+
+const ErrGeneric = `{"errno": "404", "message": "Bad Request"}`
+
+func ListenAndServe() {
+	r = echo.New()
+
+	v0 := r.Group("/api/v0")
+	v0.POST("/board", createBoard)
+	v0.GET("/board/:id", getBoard)
+	v0.PUT("/board/:id", updateBoard)
+	v0.PATCH("/board/:id", editBoard)
+	v0.GET("/board/track/:id", getBoardsFromTrack)
+	v0.DELETE("/board/:tid/:bid", deleteBoard)
+
+	r.Start(":5000")
+}
