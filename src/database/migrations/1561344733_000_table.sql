be2ba35914d9205a2a035924f97602ee4c65b4d9
Starz0r <starz0r@starz0r.com>
2019-06-24 02:26:53 -0500

    migrations: char type max is 255, and drop usign on varchar

--- src/database/migrations/1561344733_000_table.sql
+++ src/database/migrations/1561344733_000_table.sql
@@ -3,10 +3,10 @@ CREATE TABLE `boards` (
     `id` INT(8) UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE,
     `date_created` DATETIME NOT NULL DEFAULT NOW(),
     `date_modified` DATETIME,
-    `sha3` CHAR(512) NOT NULL,
+    `sha3` VARCHAR(512) NOT NULL,
     `jacket` BLOB NOT NULL,
-    `charters` VARCHAR(128) UNSIGNED NOT NULL,
-    `difficulty_name` TINYINT(3) UNSIGNED NOT NULL,
+    `charters` VARCHAR(128) NOT NULL,
+    `difficulty_name` TINYINT(2) UNSIGNED NOT NULL,
     `difficulty_rating` TINYINT(2) UNSIGNED NOT NULL,
     PRIMARY KEY (`id`),
     FOREIGN KEY (`track_id`) REFERENCES `tracks` (`id`) ON DELETE CASCADE
