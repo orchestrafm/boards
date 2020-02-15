aefa5c2b393cd367c23b0e01e22b3aec18091618
Starz0r <starz0r@starz0r.com>
2020-02-15 14:42:51 -0600

    database: redefine board type slightly to prevent json and mysql errors
    The jest is that I'm not entirely [512]byte works with json-iterator, changing it to strings seems to map it just fine then? Also, having `omitempty` on Algebraic types when they are 0 omits them from the resulting JSON. This caused a problem on the web application when trying to use that to iterate over a list of boards because it didn't exist. This is probably fixable on the web application by just assuming 0 when it is omitted, some more testing will have to be done.

--- src/database/board.go
+++ src/database/board.go
@@ -9,9 +9,9 @@ type Board struct {
 	ID               uint64    `db:"id" json:"id,omitempty"`
 	DateCreated      time.Time `db:"date_created" json:"date_created,omitempty"`
 	DateModified     time.Time `db:"date_modified" json:"date_modified,omitempty"`
-	SHA3             [512]byte `db:"sha3" json:"sha3,omitempty"`
+	SHA3             string    `db:"sha3" json:"sha3,omitempty"`
 	Jacket           []byte    `db:"jacket" json:"jacket,omitempty"`
 	Charters         string    `db:"charters" json:"charters,omitempty"`
-	DifficultyName   uint64    `db:"difficulty_name" json:"difficulty_name,omitempty"`
-	DifficultyRating uint8     `db:"difficulty_rating" json:"difficulty_rating,omitempty"`
+	DifficultyName   uint64    `db:"difficulty_name" json:"difficulty_name"`
+	DifficultyRating uint8     `db:"difficulty_rating" json:"difficulty_rating"`
 }
