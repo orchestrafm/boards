3d74b2bf529c7522b2788b20db586d5213327056
Starz0r <starz0r@starz0r.com>
2020-08-11 00:29:19 -0500

    database: field on board type struct to credit illustrators for jackets

--- src/database/board.go
+++ src/database/board.go
@@ -11,6 +11,7 @@ type Board struct {
 	DateModified     time.Time `db:"date_modified" json:"date_modified,omitempty"`
 	SHA3             string    `db:"sha3" json:"sha3,omitempty"`
 	Jacket           string    `db:"jacket" json:"jacket,omitempty"`
+	Illustrators     string    `db:"illustrators" json:"illustrators,omitempty"`
 	Charters         string    `db:"charters" json:"charters,omitempty"`
 	DifficultyName   uint64    `db:"difficulty_name" json:"difficulty_name"`
 	DifficultyRating uint8     `db:"difficulty_rating" json:"difficulty_rating"`
