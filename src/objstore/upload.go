495bb771ee3d25cca876dd9a97f88b979a9a846f
Starz0r <starz0r@starz0r.com>
2020-08-11 00:32:37 -0500

    objstore: multiple options for uploading, and read file from a buffer

--- src/objstore/upload.go
+++ src/objstore/upload.go
@@ -1,21 +1,27 @@
 package objstore
 
 import (
-	"io"
+	"bytes"
 	"os"
+	"strings"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	"github.com/spidernest-go/logger"
 )
 
-func Upload(f io.Reader, fname, acl string) (string, error) {
+func Upload(f *os.File, fname, acl string, cdn bool) (string, error) {
 	uploader := s3manager.NewUploader(session_)
+
+	finfo, _ := f.Stat()
+	fsize := finfo.Size()
+	fbuf := make([]byte, fsize)
+	f.Read(fbuf)
 
 	out, err := uploader.Upload(&s3manager.UploadInput{
 		Bucket: aws.String(os.Getenv("AWS_S3_BUCKET")),
 		Key:    aws.String(fname),
-		Body:   f,
+		Body:   bytes.NewReader(fbuf),
 		ACL:    aws.String(acl),
 	})
 
@@ -25,6 +31,14 @@ func Upload(f io.Reader, fname, acl string) (string, error) {
 			Msg("File was unable to be put into Object Storage.")
 
 		return "", err
+	}
+
+	//HACK: there should be a better way to get the cdn url
+	if cdn == true {
+		out.Location = strings.Replace(out.Location,
+			"nyc3.digitaloceanspaces.com",
+			"nyc3.cdn.digitaloceanspaces.com",
+			1)
 	}
 
 	return out.Location, nil
