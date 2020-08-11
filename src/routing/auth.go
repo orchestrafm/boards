d6ad6413ce176fdd5ea3b1e4366a765cc459af81
Starz0r <starz0r@starz0r.com>
2020-08-10 20:45:13 -0500

    routing: allow checking for a user's role

--- src/routing/auth.go
+++ src/routing/auth.go
@@ -1,6 +1,7 @@
 package routing
 
 import (
+	"errors"
 	"net/http"
 	"strings"
 
@@ -14,6 +15,11 @@ type jwtExtendedClaims struct {
 	RealmAccess struct {
 		Roles []string `json:"roles,omitempty"`
 	} `json:"realm_access,omitempty"`
+	ResourceAccess struct {
+		ApplicationServices struct {
+			Roles []string `json:"roles,omitempty"`
+		} `json:"application-services,omitempty"`
+	} `json:"resource_access,omitempty"`
 	Scope             string `json:"scope"`
 	PreferredUsername string `json:"preferred_username,omitempty"`
 	Audience          string `json:"aud,omitempty"`
@@ -26,9 +32,13 @@ type jwtExtendedClaims struct {
 	//jwt.StandardClaims
 }
 
+var (
+	ErrAuthParse   = errors.New("authorization token could not be parsed")
+	ErrPermissions = errors.New("insufficient permissions")
+)
+
 func decodeToClaims(src, dst interface{}) error {
 	//BUGFIX: Introduced in b7fdf80, I'd like to get rid of this function
-	//BUGFIX: commit is related to tracks microservice
 	dec, err := mapstructure.NewDecoder(&mapstructure.DecoderConfig{
 		TagName: "json",
 		Result:  dst,
@@ -47,6 +57,29 @@ func decodeToClaims(src, dst interface{}) error {
 		return err
 	}
 	return nil
+}
+
+func HasRole(c echo.Context, reqrole string) bool {
+	user := c.Get("user").(*jwt.Token)
+	claims := new(jwtExtendedClaims)
+	err := decodeToClaims(user.Claims, claims)
+	if err != nil {
+		logger.Warn().
+			Err(err).
+			Msg("Claims were malformed or nonexistant, returning false.")
+		return false
+	}
+
+	for _, role := range claims.
+		ResourceAccess.
+		ApplicationServices.
+		Roles {
+		if role == reqrole {
+			return true
+		}
+	}
+
+	return false
 }
 
 func FullAuthCheck(c echo.Context) error {
